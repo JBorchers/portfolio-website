83cdd305d81e00228b02e62e7d6dfe5b02df9ea8
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-11-22 18:16:40 -0700

    added new icon for hobbies section

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
 $('.nav-link').on('click',function() {
   $('.navbar-collapse').collapse('hide');
 });
+
+
+$(function(){  // $(document).ready shorthand
+  $('.monster').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var top_of_object = $(this).position().top + $(this).outerHeight();
+            var top_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( top_of_window > top_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
